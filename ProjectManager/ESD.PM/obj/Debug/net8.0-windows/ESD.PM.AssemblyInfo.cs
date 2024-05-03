d345f54b074c08b07f6492521cddfc0e76b81f48
Vitali <renecatemaan@gmail.com>
2024-05-03 12:13:19 +0300

    Preparations for DB

--- ProjectManager/ESD.PM/obj/Debug/net8.0-windows/ESD.PM.AssemblyInfo.cs
+++ ProjectManager/ESD.PM/obj/Debug/net8.0-windows/ESD.PM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6dd06af81362f556fbb2934e482dac221bde11e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProjectManager/ESD.PM/obj/Debug/net8.0-windows/Views/ShellView.g.i.cs
+++ ProjectManager/ESD.PM/obj/Debug/net8.0-windows/Views/ShellView.g.i.cs
@@ -82,7 +82,7 @@ namespace ESD.PM.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ESD.PM;component/views/shellview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ESD.PM;V1.0.0.0;component/views/shellview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\ShellView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
