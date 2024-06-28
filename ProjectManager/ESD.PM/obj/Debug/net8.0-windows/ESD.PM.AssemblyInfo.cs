d9aca64b8c7b3d0576422732cff4121a27336aa7
Vitali <renecatemaan@gmail.com>
2024-06-28 14:26:47 +0300

    Folders Window

--- ProjectManager/ESD.PM/obj/Debug/net8.0-windows/ESD.PM.AssemblyInfo.cs
+++ ProjectManager/ESD.PM/obj/Debug/net8.0-windows/ESD.PM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03105f6530c3421200e0ce1c786265c1425ed690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
