b6f51ff72ee091582700a673c01be2b9a2867387
Vitali <renecatemaan@gmail.com>
2024-07-02 16:14:43 +0300

    Design updated

--- ProjectManager/ESD.PM/obj/Debug/net8.0-windows/ESD.PM.AssemblyInfo.cs
+++ ProjectManager/ESD.PM/obj/Debug/net8.0-windows/ESD.PM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9cc51663cbf34e14b787545cf7d3b1222ec365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c54027040f13fee01edd4928409e31db4f4fa73")]
 [assembly: System.Reflection.AssemblyProductAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
