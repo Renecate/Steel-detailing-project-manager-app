9400264a644bee0f8d398e31ecb8e9608f0d3875
Vitali <renecatemaan@gmail.com>
2024-07-23 17:36:36 +0300

    Нижняя строка динамическая

--- ProjectManager/ESD.PM/obj/Debug/net8.0-windows/ESD.PM.AssemblyInfo.cs
+++ ProjectManager/ESD.PM/obj/Debug/net8.0-windows/ESD.PM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d4649da9857a69d4cd0db7e9878460f9db22eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df626fcd355eae3a6b398399ce40f303c7e0662")]
 [assembly: System.Reflection.AssemblyProductAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ESD.PM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
